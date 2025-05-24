d8b465918aed8f7190ca0917a13c9c3837d06259
Kehn <ezeokpokc@gmail.com>
2025-05-24 09:45:03 +0100

    all roads lead to /home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,31 +19,57 @@ const Navbar = () => {
       <div className={styles.logo}>
         <Link to="/">Bandage</Link>
       </div>
-      <div className={styles.hamburger} onClick={toggleMenu}>
-        <span className={styles.bar}></span>
-        <span className={styles.bar}></span>
-        <span className={styles.bar}></span>
-      </div>
-      <ul className={`${styles.links} ${menuOpen ? styles.mobileMenu : ''}`}>
-        <li><Link to="/">Home</Link></li>
-        <li className={styles.dropdown}>
-          <Link to="/shop">Shop</Link>
-          <span className={styles.toggle}>▼</span>
-        </li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/pages">Pages</Link></li>
-      </ul>
-      <div className={styles.icons}>
-        <Link to="/login"><FaUser /> Login / Register</Link>
+
+      <div className={styles.mobileIcons}>
         <Link to=""><FaSearch /></Link>
         <Link to="/cart">
-        <div className={styles.iconWithBadge}>
-          <FaShoppingCart />
-          <span className={styles.count}>{cartItemCount}</span>
-        </div>
+      <div className={styles.iconWithBadge}>
+      <FaShoppingCart />
+      <span className={styles.count}>{cartItemCount}</span>
+      </div>
         </Link>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </div>
+      </div>
+
+
+      <ul className={styles.links}>
+        <li><Link to="/">Home</Link></li>
+        <li className={styles.dropdown}>
+          <Link to="/">Shop</Link>
+          <span className={styles.toggle}>▼</span>
+        </li>
+        <li><Link to="/">About</Link></li>
+        <li><Link to="/">Blog</Link></li>
+        <li><Link to="/">Contact</Link></li>
+        <li><Link to="/">Pages</Link></li>
+      </ul>
+
+      {menuOpen && (
+        <ul className={styles.mobileMenu}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">Shop</Link></li>
+          <li><Link to="/">Product</Link></li>
+          <li><Link to="/">Pricing</Link></li>
+          <li><Link to="/">Contact</Link></li>
+        </ul>
+      )}
+
+      <div className={styles.icons}>
+        <Link to="/" className={styles.login}><FaUser /> Login / Register</Link>
+
+        <Link to=""><FaSearch /></Link>
+
+        <Link to="/cart">
+          <div className={styles.iconWithBadge}>
+            <FaShoppingCart />
+            <span className={styles.count}>{cartItemCount}</span>
+          </div>
+        </Link>
+
         <div className={styles.iconWithBadge}>
           <FaHeart />
           <span className={styles.count}>0</span>
